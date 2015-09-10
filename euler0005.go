56a64db23646f1ff65d0a34b0b614320b055d362
Daniel Tereschenko <daniel@tereschenko.info>
2015-09-10 23:49:54 +0300

    problem 5

--- euler0005.go
+++ euler0005.go
@@ -1,9 +1,9 @@
 // euler0005 project main.go
 /*
-   What is the smallest positive number that is evenly divisible 
+   What is the smallest positive number that is evenly divisible
    by all of the numbers from 1 to 20?
 
-   Near straightforward brutforce
+   Prime factorization instead of straightforward brutforce
 
    @author Dan Tereschenko <dan@tereschenko.info>
    @copyright 2015
@@ -12,22 +12,58 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
-func isFound(i int) bool {
-	for x := 11; x <= 20; x ++ {// skip 1-10 as dividers in 11-20
-		if i%x != 0 {
-			return false
+//func isFound(i int) bool {
+//    for x := 11; x <= 20; x++ {
+//        if i%x != 0 {
+//            return false
+//        }
+//    }
+//    return true
+//}
+
+//func main() {
+//    for i := 20; ; i +=2 {
+//        if isFound(i) {
+//            fmt.Println(i)
+//            break
+//        }
+//    }
+//}
+
+var prime = []int{2, 3, 5, 7, 11, 13, 17, 19}
+
+func getFactors(n int) map[int]int {
+	result := make(map[int]int)
+	n1 := n
+	for _, f := range prime {
+		for {
+			if remainder := n1 % f; remainder == 0 {
+				n1 = n1 / f
+				result[f]++
+			} else {
+				break
+			}
 		}
 	}
-	return true
+	return result
 }
 
 func main() {
-	for i := 20; ; i += 2 {
-		if isFound(i) {
-			fmt.Println(i)
-			break
+	r := 1
+	factors := map[int]int{}
+	for i := 11; i <= 20; i++ {
+		v := getFactors(i)
+		for key, val := range v {
+			if factors[key] < val {
+				factors[key] = val
+			}
 		}
 	}
+	for key, val := range factors {
+		r = r * int(math.Pow(float64(key), float64(val)))
+	}
+	fmt.Println(r)
 }
