91b6030c3e53e1d523f040a8b0ab013ba95bae1b
Daniel Tereschenko <daniel@tereschenko.info>
2015-09-11 00:21:31 +0300

    Update euler0005.go

--- euler0005.go
+++ euler0005.go
@@ -3,7 +3,7 @@
    What is the smallest positive number that is evenly divisible
    by all of the numbers from 1 to 20?
 
-   Prime factorization instead of straightforward brutforce
+   Prime factorization instead of straightforward bruteforce
 
    @author Dan Tereschenko <dan@tereschenko.info>
    @copyright 2015
